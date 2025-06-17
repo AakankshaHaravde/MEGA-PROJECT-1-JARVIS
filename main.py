a1b5add0572abd40e3ce000d3d8a0f277835c4ea
AakankshaHaravde <aakankshaharavde@gmail.com>
2025-06-18 03:53:17 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import speech_recognition as sr
+import webbrowser 
+import pyttsx3
+
+# pip install pocketsphinx
+# pip install SpeechRecognition
+# pip install pyttsx3
+
+recognizer = sr.Recognizer() # is used to recognize speech
+engine = pyttsx3.init() # is used to convert text to speech 
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+if __name__ == "__main__":
+    speak("Initializing Jarivis....")
+    while True:
+        # Listen for the wake word 'Jarvis'
+        # obtain audio from the microphone
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("Listening...")
+            audio = r.listen(source, timeout = 2) # listen for the audio input from the microphone
+            
+        print("Recognizing...")
+        # recognize speech using Google  
+
+        # recognize speech using Google 
+        try:
+            command = r.recognize_google(audio)    
+            print(command)
+            print("Jarvis thinks you said " + r.recognize_sphinx(audio))
+        except sr.UnknownValueError:
+            print("Jarvis could not understand audio")
+        except sr.RequestError as e:
+            print("Sphinx error; {0}".format(e))
+ 
+
